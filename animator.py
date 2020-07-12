6f3b208d89337dcf8bd73940a72bff289b738666
Vojtech Semecky <vojtech@semecky.cz>
2020-07-12 16:25:00 +0200

    Animator: latent_interpolation_clip() added

--- animator.py
+++ animator.py
@@ -15,7 +15,7 @@ imageio.plugins.ffmpeg.download()  # Download ffmpeg if not installed
 
 
 #
-# Loads pretrained network from pkl. Result is cached for future use.
+# Loads pre-trained network from pkl. Result is cached for future use.
 #
 def load_network(pkl):
     if pkl in networks_cache.keys():
@@ -38,9 +38,117 @@ def get_filename(prefix="video", time=None, psi=None):
     if time:
         file_name += " - {}sec".format(time)
     if psi:
-        file_name += " - psi={}".format(psi)
+        file_name += " - psi={:03d}".format(100 * psi)
+
+    file_name += ".mp4"
 
     return file_name
+
+
+import pickle
+import numpy as np
+import dnnlib
+import dnnlib.tflib as tflib
+import PIL.Image
+import moviepy.editor
+from moviepy.editor import *
+from training.misc import create_image_grid
+
+tflib.init_tf()
+
+
+# from https://colab.research.google.com/drive/1ShgW6wohEFQtqs_znMna3dzrcVoABKIH
+def generate_zs_from_seeds(seeds, Gs):
+    zs = []
+    enam = enumerate(seeds)
+    for seed_idx, seed in enumerate(seeds):
+        rnd = np.random.RandomState(seed)
+        z = rnd.randn(1, *Gs.input_shape[1:])  # [minibatch, component]
+        zs.append(z)
+    return zs
+
+
+def generate_latent_images(zs, truncation_psi, save_npy, prefix):
+    Gs_kwargs = dnnlib.EasyDict()
+    Gs_kwargs.output_transform = dict(func=tflib.convert_images_to_uint8, nchw_to_nhwc=True)
+    Gs_kwargs.randomize_noise = False
+    if not isinstance(truncation_psi, list):
+        truncation_psi = [truncation_psi] * len(zs)
+
+    for z_idx, z in enumerate(zs):
+        if isinstance(z, list):
+            z = np.array(z).reshape(1, 512)
+        elif isinstance(z, np.ndarray):
+            z.reshape(1, 512)
+        print('Generating image for step %d/%d ...' % (z_idx, len(zs)))
+        Gs_kwargs.truncation_psi = truncation_psi[z_idx]
+        noise_rnd = np.random.RandomState(1)  # fix noise
+        tflib.set_vars({var: noise_rnd.randn(*var.shape.as_list()) for var in noise_vars})  # [height, width]
+        images = Gs.run(z, None, **Gs_kwargs)  # [minibatch, height, width, channel]
+        PIL.Image.fromarray(images[0], 'RGB').save(dnnlib.make_run_dir_path('%s%05d.png' % (prefix, z_idx)))
+        if save_npy:
+            np.save(dnnlib.make_run_dir_path('%s%05d.npy' % (prefix, z_idx)), z)
+
+
+def line_interpolate(zs, steps):
+    out = []
+    for i in range(len(zs) - 1):
+        for index in range(steps):
+            fraction = index / float(steps)
+            out.append(zs[i + 1] * fraction + zs[i] * (1 - fraction))
+    return out
+
+
+#
+# @see sasdfrom https://github.com/dvschultz/stylegan2
+#
+def latent_interpolation_clip(pkl, psi=0.5, mp4_fps=30, duration=60, seeds=[]):
+    # Loading neurals
+    _G, _D, Gs = load_network(pkl)
+
+    noise_vars = [var for name, var in Gs.components.synthesis.vars.items() if name.startswith('noise')]
+
+    zs = generate_zs_from_seeds(seeds, Gs)
+
+    num_frames = int(np.rint(duration * mp4_fps))
+    number_of_steps = int(num_frames / (len(zs) - 1)) +1  # @todo Prejmenovat na num_steps nebo steps_count/frames_count
+    points = line_interpolate(zs, number_of_steps)
+
+    # Generate_latent_images()
+    Gs_kwargs = dnnlib.EasyDict()
+    Gs_kwargs.output_transform = dict(func=tflib.convert_images_to_uint8, nchw_to_nhwc=True)
+    Gs_kwargs.randomize_noise = False
+
+    # Frame generation func for moviepy
+    def make_frame(t):
+        frame_idx = int(np.clip(np.round(t * mp4_fps), 0, num_frames - 1))
+
+        # TIP: Oddebugovat run_generator,  co mu sem leze, zejmena len(zx)
+        z_idx = frame_idx
+        z = points[z_idx]
+
+        # Puvodni loop
+        if isinstance(z, list):
+            z = np.array(z).reshape(1, 512)
+        elif isinstance(z, np.ndarray):
+            z.reshape(1, 512)
+
+        Gs_kwargs.truncation_psi = psi
+        noise_rnd = np.random.RandomState(1)  # fix noise
+        tflib.set_vars({var: noise_rnd.randn(*var.shape.as_list()) for var in noise_vars})  # [height, width]
+        images = Gs.run(z, None, **Gs_kwargs)  # [minibatch, height, width, channel]
+
+        # @todo Zbavit se gridu, kdyz potrebujeme jen jeden obrazek
+        images = images.transpose(0, 3, 1, 2)  # NHWC -> NCHW
+        grid = create_image_grid(images, [1, 1]).transpose(1, 2, 0)  # HWC
+        # if grid.shape[2] == 1:
+        #     grid = grid.repeat(3, 2)  # grayscale => RGB
+        return grid
+
+    # Return clip
+    clip = moviepy.editor.VideoClip(make_frame, duration=duration)
+    return clip
+
 
 #
 # Generates latent walk VideoClip
